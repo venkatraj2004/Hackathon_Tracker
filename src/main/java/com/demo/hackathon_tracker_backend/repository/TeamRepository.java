f2cea2d4c286c5a1eefba3aa083a2d60eb476312
Venkatraj <blvraj2004@gmail.com>
2026-03-12 11:52:44 +0530

    summa

--- src/main/java/com/demo/hackathon_tracker_backend/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.hackathon_tracker_backend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.hackathon_tracker_backend.entity.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
